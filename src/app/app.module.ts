1828261ef3ef3a688432d05a145235babc2906dd
Md_Ibrahim_Khan <ibrahim.khan@selise.ch>
2022-07-25 11:27:29 -0700

    Page not found implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatTooltipModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { HomeComponent } from './Components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatIconModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
